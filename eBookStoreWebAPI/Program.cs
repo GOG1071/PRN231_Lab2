ed33ec10908b7efbb96abe4dc32d9b03d3e37ba9
TungGPOCG <goldendarkness1071@gmail.com>
2023-10-16 00:54:42 +0700

    Swagger

--- eBookStoreWebAPI/Program.cs
+++ eBookStoreWebAPI/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // builder.Services.AddControllers();
 // // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 var modelBuilder = new ODataConventionModelBuilder();
 modelBuilder.EntitySet<Book>("Books");
@@ -31,6 +31,11 @@ builder.Services.AddControllers().AddOData(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseRouting();
 
